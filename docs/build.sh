318d8c9b2962abc8da0234ddde60a3514d7f4e06
Jan Hargreaves <242093156+impactcrew@users.noreply.github.com>
2026-03-11 18:38:17 +0700

    Add build step for landing page minification
    Source files in docs/src/, minified output in docs/.
    Run docs/build.sh to rebuild.

---
+++ docs/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Minify docs/src/ -> docs/ for deployment
+# Requires: npm i -g html-minifier-terser csso-cli
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+html-minifier-terser \
+  --collapse-whitespace \
+  --remove-comments \
+  --remove-redundant-attributes \
+  --minify-css true \
+  --minify-js true \
+  "$DIR/src/index.html" -o "$DIR/index.html"
+
+csso "$DIR/src/style.css" -o "$DIR/style.css"
+
+echo "Built:"
+wc -c "$DIR/src/index.html" "$DIR/src/style.css" | tail -1 | awk '{print "  src: " $1 " bytes"}'
+wc -c "$DIR/index.html" "$DIR/style.css" | tail -1 | awk '{print "  out: " $1 " bytes"}'
